8323d94f9dc3c680be47f3483eac47ab659d513f
Tan Cuong <cuongnt@giigaa.com>
2019-03-21 11:28:04 +0700

    Finish CoreDataPickerTF.

--- GroceryDude/CoreDataPickerTF.m
+++ GroceryDude/CoreDataPickerTF.m
@@ -64,8 +64,101 @@ rowHeightForComponent:(NSInteger)component {
 
 #pragma mark - INTERACTION
 
+- (void)done {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  [self resignFirstResponder];
+}
+
+- (void)clear {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  [[self pickerDelegate] selectedObjectClearedForPickerTF:self];
+  [self resignFirstResponder];
+}
+
 #pragma mark - DATA
 
+- (void)fetch {
+  [NSException raise:NSInternalInconsistencyException
+              format:@"Method %@ must be overriden before being invoked", NSStringFromSelector(_cmd)];
+}
+
+- (void)selectDefaultRow {
+  [NSException raise:NSInternalInconsistencyException
+              format:@"Method %@ must be overriden before being invoked", NSStringFromSelector(_cmd)];
+}
+
 #pragma mark - VIEW
+
+- (UIView *)createInputView {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  [self setPicker:[[UIPickerView alloc] initWithFrame:CGRectZero]];
+  [[self picker] setShowsSelectionIndicator:YES];
+  [[self picker] setAutoresizingMask:UIViewAutoresizingFlexibleHeight];
+  [[self picker] setDelegate:self];
+  [[self picker] setDataSource:self];
+  [self fetch];
+  return [self picker];
+}
+
+- (UIView *)createInputAccessaryView {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  [self setShowToolbar:YES];
+  if (![self toolbar] && [self showToolbar]) {
+    [self setToolbar:[[UIToolbar alloc] init]];
+    [[self toolbar] setBarStyle:UIBarStyleBlackTranslucent];
+    [[self toolbar] setAutoresizingMask:UIViewAutoresizingFlexibleHeight];
+    [[self toolbar] sizeToFit];
+    CGRect frame = [[self toolbar] frame];
+    frame.size.height = 44.0f;
+    [[self toolbar] setFrame:frame];
+    UIBarButtonItem *clearBtn = [[UIBarButtonItem alloc] initWithTitle:@"Clear"
+                                                                 style:UIBarButtonItemStylePlain
+                                                                target:self
+                                                                action:@selector(clear)];
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                            target:nil
+                                                                            action:nil];
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                                target:self
+                                                                                action:@selector(done)];
+    NSArray *buttonsArray = @[clearBtn, spacer, doneButton];
+    [[self toolbar] setItems:buttonsArray];
+  }
+  return [self toolbar];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  if (self = [super initWithFrame:frame]) {
+    [self setInputView:[self createInputView]];
+    [self setInputAccessoryView:[self createInputAccessaryView]];
+  }
+  return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+  if (self = [super initWithCoder:coder]) {
+    [self setInputView:[self createInputView]];
+    [self setInputAccessoryView:[self createInputAccessaryView]];
+  }
+  return self;
+}
+
+- (void)deviceDidRotate:(NSNotification *)notification {
+#if DEBUG
+  NSLog(@"Running %@, '%@'", [self class], NSStringFromSelector(_cmd));
+#endif
+  [[self picker] setNeedsLayout];
+}
 
 @end
